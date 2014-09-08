3401772385619abf937ac736bb576015998ffcea
Chris Zubak-Skees <chriszs@gmail.com>
2014-09-07 23:32:04 -0400

    Made it work on mobile?

--- src/script/script.js
+++ src/script/script.js
@@ -15,4 +15,12 @@
     top.on('mousemove',function () {
         top.style('background-color',d3.hsl(hueScale(d3.event.pageY),saturationScale(d3.event.pageX),0.5));
     });
+
+    top.on('touchmove',function () {
+        top.style('background-color',d3.hsl(hueScale(document.body.scrollTop),saturationScale(d3.event.pageX),0.5));
+    });
+
+    top.on('scroll',function () {
+        top.style('background-color',d3.hsl(hueScale(document.body.scrollTop),saturationScale(d3.event.pageX),0.5));
+    });
 })();
