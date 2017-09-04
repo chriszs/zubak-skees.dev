b5d9c414227166b173905ecf8f49ae469d6bf737
Chris Zubak-Skees <chriszs@gmail.com>
2017-09-04 00:42:01 -0400

    Update with accent bar

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,9 @@
 (function () {
-    var top = d3.select('.top');
+    var top = d3.select('.top'),
+        body = d3.select('body');
 
-    var width = top.node().offsetWidth,
-        height = top.node().offsetHeight;
+    var width = body.node().offsetWidth,
+        height = body.node().offsetHeight;
 
     var hueScale = d3.scale.linear()
                         .domain([0,height])
@@ -12,15 +13,15 @@
                         .domain([0,width])
                         .range([0.3,1]);
 
-    top.on('mousemove',function () {
+    body.on('mousemove',function () {
         top.style('background-color',d3.hsl(hueScale(d3.event.pageY),saturationScale(d3.event.pageX),0.5));
     });
 
-    top.on('touchmove',function () {
+    body.on('touchmove',function () {
         top.style('background-color',d3.hsl(hueScale(document.body.scrollTop),saturationScale(d3.event.pageX),0.5));
     });
 
-    top.on('scroll',function () {
+    body.on('scroll',function () {
         top.style('background-color',d3.hsl(hueScale(document.body.scrollTop),saturationScale(d3.event.pageX),0.5));
     });
 })();
